d57f32f257b5609a37e4352413c10cc4a3e7c9a4
Federico S <federico97sala@gmail.com>
2020-03-30 13:15:18 +0200

    Aggiunto file python

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 n = int(input("Inserisci un numero => "))
 m = int(input("Inserisci un numero => "))
-print("Ora calcolero' il maggiore tra i due")
+print("Ora calcolero' il maggiore tra i due e se esso e' divisibile per l'altro")
+print(" ")
 
 if n == m:
 	print("I due numeri sono uguali")
 elif n > m:
-	print("Il numero maggiore tra i due e'", n)
+	if n%m == 0:
+		print("Il numero maggiore tra i due e'", n)
+		print(n, " e' divisibile per ", m)
+	else:
+		print("Il numero maggiore tra i due e'", n)
+		print(n, " non e' divisibile per ", m)
 elif n < m:
-	print("Il numero maggiore tra due e'", m)
+	if m%n == 0:
+		print("Il numero maggiore tra due e'", m)
+		print(m, " e' divisibile per ", n)
+	else:
+		print("il numero maggiore tra i due e'", m)
+		print(m , " non e' divisibile per ", n)
