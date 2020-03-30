d6841c7c7efd97e260825392ce4bca849f969d7f
Federico S <federico97sala@gmail.com>
2020-03-30 13:10:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Federico Sala
+
+Aggiunti file al .gitignore 
+
+Aggiunto file main.py
+
---
+++ main.py
@@ -0,0 +1,10 @@
+n = int(input("Inserisci un numero => "))
+m = int(input("Inserisci un numero => "))
+print("Ora calcolero' il maggiore tra i due")
+
+if n == m:
+	print("I due numeri sono uguali")
+elif n > m:
+	print("Il numero maggiore tra i due e'", n)
+elif n < m:
+	print("Il numero maggiore tra due e'", m)
